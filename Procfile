d1dc54a947b84af61ce263f7885451f2b83baae1
Wesley Borges Da Conceicao <wesley.conceicao@sp01.local>
2019-07-23 12:37:26 -0300

    Configure The App

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn apiPaintBall.wsgi --log-file -
--- apiPaintBall/settings.py
+++ apiPaintBall/settings.py
@@ -15,6 +15,8 @@ import datetime
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,13 +25,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3o7h44l92yzvv(=6-g0fom-_740v8sxl1=9gszmuxv%6cc2loq'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = '3o7h44l92yzvv(=6-g0fom-_740v8sxl1=9gszmuxv%6cc2loq'
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apipaintball.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -87,12 +92,17 @@ WSGI_APPLICATION = 'apiPaintBall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
